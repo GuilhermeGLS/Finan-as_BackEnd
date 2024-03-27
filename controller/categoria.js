ef5f873e18e1639b084860d45bf8a7696e3d67b7
Guilherme Lima e Silva <guilhermelimaesilva.p@gmail.com>
2024-03-27 15:01:41 -0300

    Hotfix: Refatorando o arquivo categoria dentro de controller, pois só caia no catch (Após alguns videos conseguir consertar)

--- controller/categoria.js
+++ controller/categoria.js
@@ -1,62 +1,65 @@
 const sequelize = require('sequelize');
-const model = require("../models/");
-const { response } = require('express');
-const categoria = model.categoria;
-
-const create = async (req, res) => {
-    try {
-        const decricao = req.body
-        const Categoria = await categoria.create({
-            decricao
-        })
-
-        return res.json({ msg: "Categoria cadastrar com Sucesso!" });
-    } catch (error) {
-        return res.json({ msg: "Não foi Possível cadastrar a categoria" + error });
-    }
-}
-
-const update = async (req, res) => {
-    try {
-        const { id } = req.params;
-
-        const decricao = req.body
-
-        const Categoria = await categoria.update({
-            decricao
-
-        }, { where: { id } });
-
-        return res.json({ msg: "Categoria Alterada com Sucesso!" });
-
-    } catch (error) {
-        return res.json({ msg: "Não foi Possível alterar a categoria" + error });
-    }
-}
-
-const findAll = async (req, res) => {
-    try {
-        const { page } = req.params;
-        const limite = 5;
-
-        const Categoria = await categoria.findAndCountAll({
-            order: [
-                ['id', 'ASC']
-            ],
-            limit: limite,
-            offset: parseInt(page)
-        })
-
-        return res.json(Categoria)
-    } catch (error) {
-        return res.json("erro ao listar categorias" + error)
-
-    }
-}
-
+const model = require('../models');
+const categoria = model.Categoria;
 
 module.exports = {
-    create,
-    update,
-    findAll
+    async create(req, res) {
+        try {
+            const {
+                descricao
+            } = req.body
+
+            const Categoria = await categoria.create({
+                descricao
+            });
+
+            return res.json({ msg: "Categoria cadastrada com sucesso!! " });
+
+        } catch (error) {
+            return res.json({ msg: "Não foi possivel cadastrar a categoria " + error });
+
+        }
+    },
+
+    async update(req, res) {
+        try {
+            const { id } = req.params;
+
+            const {
+                descricao
+            } = req.body
+
+            const Categoria = await categoria.update({
+                descricao
+
+            }, { where: { id } });
+
+            return res.json({ msg: "Categoria alterada com sucesso!! " });
+
+
+        } catch (error) {
+            return res.json({ msg: "Não foi possivel alterar a categoria " + error });
+        }
+    },
+
+    async findAll(req, res) {
+        try {
+            const { page } = req.params;
+            const limite = 5;
+
+            const Categoria = await categoria.findAndCountAll({
+                order: [
+                    ['id', 'ASC']
+                ],
+                limit: limite,
+                offset: parseInt(page)
+            })
+
+            return res.json(Categoria);
+
+        } catch (error) {
+            return res.json("Erro ao listar categorias " + error);
+        }
+    }
+
 }

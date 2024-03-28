fac907d85c44149db24c1b14f5174e45e9dfa90c
Guilherme Lima e Silva <guilhermelimaesilva.p@gmail.com>
2024-03-28 19:14:31 -0300

    Add: Novas rotas de listagem, update e delete

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
-const express = require('express')
-const app = express()
-const router = require("./routes/router")
+const express = require('express');
+const app = express();
+const router = require("./routes/router");
+const cors = require('cors');
 
 app.use(express.urlencoded({extended: false}))
 app.use(express.json());
+app.use(cors())
 app.use(router);
 
 app.listen(8080, (req, res) => {
--- routes/router.js
+++ routes/router.js
@@ -11,8 +11,21 @@ router.put('/atualizar/categoria/:id', categoria.update);
 
 // Rotas de Financas
 router.post('/criar/financa', financa.create);
+
+// Listando todas pela página
 router.get('/listar/financa/:page', financa.findAll);
+
+// Listando por id
+router.get('/pesquisar/financa/categoria_id/:id', financa.findById);
+
+// Listar por data
+router.get('/listar/financa/dataInicial/:dataInicial/dataFinal/:dataFinal/page/:page', financa.findAllDate);
+
+// Atualizando
 router.put('/atualizar/financa/:id', financa.update);
+
+// Deleteando
+router.delete('/deletar/financa/:id', financa.delete)
 module.exports = router;
 
 
